3328ff68a5ab32b3d163f8a9ddf7086f6a7ba77b
samp830 <samp.830@gmail.com>
2017-02-27 13:14:40 -0800

    initial commit

---
+++ receipts/api.js
@@ -0,0 +1,120 @@
+// Copyright 2015-2016, Google, Inc.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+'use strict';
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const config = require('../config');
+
+function getModel () {
+  return require(`./model-${config.get('DATA_BACKEND')}`);
+}
+
+const router = express.Router();
+
+// Automatically parse request body as JSON
+router.use(bodyParser.json());
+
+/**
+ * GET /api/receipts
+ *
+ * Retrieve a page of receipts (up to ten at a time).
+ */
+router.get('/', (req, res, next) => {
+  getModel().list(10, req.query.pageToken, (err, entities, cursor) => {
+    if (err) {
+      next(err);
+      return;
+    }
+    res.json({
+      items: entities,
+      nextPageToken: cursor
+    });
+  });
+});
+
+/**
+ * POST /api/receipts
+ *
+ * Create a new receipt.
+ */
+router.post('/', (req, res, next) => {
+  getModel().create(req.body, (err, entity) => {
+    if (err) {
+      next(err);
+      return;
+    }
+    res.json(entity);
+  });
+});
+
+/**
+ * GET /api/books/:id
+ *
+ * Retrieve a receipt.
+ */
+router.get('/:receipt', (req, res, next) => {
+  getModel().read(req.params.receipt, (err, entity) => {
+    if (err) {
+      next(err);
+      return;
+    }
+    res.json(entity);
+  });
+});
+
+/**
+ * PUT /api/receipts/:id
+ *
+ * Update a receipt.
+ */
+router.put('/:receipt', (req, res, next) => {
+  getModel().update(req.params.receipt, req.body, (err, entity) => {
+    if (err) {
+      next(err);
+      return;
+    }
+    res.json(entity);
+  });
+});
+
+/**
+ * DELETE /api/receipts/:id
+ *
+ * Delete a receipt.
+ */
+router.delete('/:receipt', (req, res, next) => {
+  getModel().delete(req.params.receipt, (err) => {
+    if (err) {
+      next(err);
+      return;
+    }
+    res.status(200).send('OK');
+  });
+});
+
+/**
+ * Errors on "/api/receipts/*" routes.
+ */
+router.use((err, req, res, next) => {
+  // Format error and forward to generic error handler for logging and
+  // responding to the request
+  err.response = {
+    message: err.message,
+    internalCode: err.internalCode
+  };
+  next(err);
+});
+
+module.exports = router;
